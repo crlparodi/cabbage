0fed2e54fba4d20799e48e7b8fba60adc859e434
Cyril PARODI <parodicyril@gmail.com>
2024-04-10 18:50:39 +0200

    gunicorn Runtime

---
+++ run.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+/opt/env/bin/python manage.py makemigrations accounts
+/opt/env/bin/python manage.py makemigrations babysitters
+/opt/env/bin/python manage.py migrate
+/opt/env/bin/python manage.py collectstatic --no-input
+/opt/env/bin/gunicorn core.wsgi:application --log-level info --workers 3 --bind "0.0.0.0:8085"
+# /opt/env/bin/gunicorn --worker-tmp-dir /dev/shm/ --access-logfile - --workers 3 --bind unix:/run/gunicorn.sock core.wsgi:application --bind "0.0.0.0:8085"'
