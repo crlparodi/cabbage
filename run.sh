217ea11c8028fdbd6d7045898c85e1cc181f4fd9
Cyril PARODI <parodicyril@gmail.com>
2024-04-11 20:09:55 +0200

    fix run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 /opt/env/bin/python manage.py migrate
 /opt/env/bin/python manage.py collectstatic --no-input
 /usr/sbin/nginx 
-/opt/env/bin/gunicorn --worker-tmp-dir /dev/shm/ --access-logfile - --workers 3 --bind unix:/run/gunicorn.sock core.wsgi:application --bind "0.0.0.0:8085"'
+/opt/env/bin/gunicorn --worker-tmp-dir /dev/shm/ --access-logfile - --workers 3 --bind unix:/run/gunicorn.sock core.wsgi:application --bind "0.0.0.0:8085"
