b070e1d1b5b8df878462f8d14f311644165b1e2f
Cyril PARODI <parodicyril@gmail.com>
2024-04-11 20:03:36 +0200

    Tinkering with Static Files

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 /opt/env/bin/python manage.py makemigrations babysitters
 /opt/env/bin/python manage.py migrate
 /opt/env/bin/python manage.py collectstatic --no-input
-/opt/env/bin/gunicorn core.wsgi:application --log-level info --workers 3 --bind "0.0.0.0:8085"
-# /opt/env/bin/gunicorn --worker-tmp-dir /dev/shm/ --access-logfile - --workers 3 --bind unix:/run/gunicorn.sock core.wsgi:application --bind "0.0.0.0:8085"'
+/usr/sbin/nginx 
+/opt/env/bin/gunicorn --worker-tmp-dir /dev/shm/ --access-logfile - --workers 3 --bind unix:/run/gunicorn.sock core.wsgi:application --bind "0.0.0.0:8085"'
